04e0a9b7f146685d2d764eee13cf1520874cb467
JarenPOL1015 <jarapazm@espol.edu.ec>
2025-01-08 10:45:41 -0500

    Actividad finalizada

--- api/views.py
+++ api/views.py
@@ -40,3 +40,50 @@ class LandingAPI(APIView):
         
         # Devuelve el id del objeto guardado
         return Response({"id": new_resource.key}, status=status.HTTP_201_CREATED)
+
+class LandingAPIDetail(APIView):
+
+     name = 'Landing Detail API'
+
+     collection_name = 'coleccion'
+
+     def get(self, request, pk):
+        ref = db.reference(f'{self.collection_name}')
+    
+        # get: Obtiene todos los elementos de la colección
+        data = ref.get()
+
+        resultado = data.get(pk)
+
+        if resultado is None:
+            # Si no se encuentra, retornar un error 404
+            return Response({"error": "Elemento no encontrado"}, status=status.HTTP_404_NOT_FOUND)
+
+        # Devuelve el elemento encontrado
+        return Response(resultado, status=status.HTTP_200_OK)
+
+     def put(self, request, pk):
+        # Referencia a la colección
+        ref = db.reference(f'{self.collection_name}/{pk}')
+
+        # Verificar si el elemento existe
+        if not ref.get():
+            return Response({"error": "Elemento no encontrado"}, status=status.HTTP_404_NOT_FOUND)
+
+        # Actualizar los datos
+        ref.update(request.data)
+
+        return Response({"message": "Elemento actualizado exitosamente"}, status=status.HTTP_200_OK)
+
+     def delete(self, request, pk):
+        # Referencia al elemento específico
+        ref = db.reference(f'{self.collection_name}/{pk}')
+        
+        # Verificar si el elemento existe
+        if not ref.get():
+            return Response({"error": "Elemento no encontrado"}, status=status.HTTP_404_NOT_FOUND)
+
+        # Eliminar el elemento
+        ref.delete()
+
+        return Response({"message": "Elemento eliminado exitosamente"}, status=status.HTTP_200_OK)
