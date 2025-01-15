325b7aeea5483f5b4bdc6f2e3d5922ecbf4ebdca
JarenPOL1015 <jarapazm@espol.edu.ec>
2025-01-15 10:25:22 -0500

    Guia 24 Finished

--- main/views.py
+++ main/views.py
@@ -8,10 +8,11 @@ import json
 from django.http import HttpResponse
 
 # Importe el decorador login_required
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import login_required, permission_required
 
-# Restricción de acceso con @login_required
+# Restricción de acceso con @login_required y permisos con @permission_required
 @login_required
+@permission_required('main.index_viewer', raise_exception=True)
 def index(request):
     # Arme el endpoint del REST API
     current_url = request.build_absolute_uri()
