809a5413bb8bc7bc49e910f51f6f6102c70bc74f
Ariel-Vargas <arielvargas2004@hotmail.com>
2025-01-21 22:49:12 -0500

    Actualización de etiquetas y contenido del grafico de linea

--- static/js/charts-lines.js
+++ static/js/charts-lines.js
@@ -4,10 +4,10 @@
 const lineConfig = {
   type: 'line',
   data: {
-    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+    labels: ["January", "February", "March", "April", "May", "June", "July"],
     datasets: [
       {
-        label: 'Organic',
+        label: 'content',
         /**
          * These colors come from Tailwind CSS palette
          * https://tailwindcss.com/docs/customizing-colors/#default-color-palette
@@ -16,17 +16,6 @@ const lineConfig = {
         borderColor: '#0694a2',
         data: [43, 48, 40, 54, 67, 73, 70],
         fill: false,
-      },
-      {
-        label: 'Paid',
-        fill: false,
-        /**
-         * These colors come from Tailwind CSS palette
-         * https://tailwindcss.com/docs/customizing-colors/#default-color-palette
-         */
-        backgroundColor: '#7e3af2',
-        borderColor: '#7e3af2',
-        data: [24, 50, 64, 74, 52, 51, 65],
       },
     ],
   },
@@ -52,7 +41,7 @@ const lineConfig = {
         display: true,
         scaleLabel: {
           display: true,
-          labelString: 'Month',
+          labelString: 'Day',
         },
       },
       y: {
@@ -69,3 +58,52 @@ const lineConfig = {
 // change this to the id of your chart element in HMTL
 const lineCtx = document.getElementById('line')
 window.myLine = new Chart(lineCtx, lineConfig)
+
+countCommentsByDay = (data) => {
+
+  // Inicializar contadores por rango de horas
+  const labels = ["Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado", "Domingo"];
+  const counts = [0, 0, 0, 0, 0, 0, 0];
+
+  Object.values(data).forEach(record => {
+    const savedDate = record.saved;
+    if (!savedDate) return;
+
+    const date = new Date(savedDate);
+    if(date.getDay() == 1){
+      counts[0]++;
+    }else if(date.getDay() == 2){
+      counts[1]++;
+    }else if(date.getDay() == 3){
+      counts[2]++;
+    }else if(date.getDay() == 4){
+      counts[3]++;
+    }else if(date.getDay() == 5){
+      counts[4]++;
+    }else if(date.getDay() == 6){
+      counts[5]++;
+    }else{
+      counts[6]++;
+    }
+  });
+
+  return { labels, counts };
+}
+
+updateLineChart = () => {
+  fetch('/api/v1/landing')
+      .then(response => response.json())
+      .then(data => {
+          const { labels, counts } = countCommentsByDay(data);
+
+          // Actualizar el gráfico
+          window.myLine.data.labels = labels;
+          window.myLine.data.datasets[0].data = counts;
+          window.myLine.update();
+      })
+      .catch(error => console.error('Error:', error));
+};
+
+updateLineChart();
+
+
